0d9da8241182c16eb5fc18f13b54266fddac03d6
danya <danyakresichenko0709@gmail.com>
2025-04-05 09:46:35 +0300

    urls users cats news

--- backend/rniirs/urls.py
+++ backend/rniirs/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from users.views import CustomUserAPIView
+from news.views import NewsAPIView, CategoryAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('api/users/', CustomUserAPIView.as_view(), name='users'),
+    path('api/categories/', CategoryAPIView.as_view(), name='categories'),
+    path('api/news/', NewsAPIView.as_view(), name='news'),
 
 ]
